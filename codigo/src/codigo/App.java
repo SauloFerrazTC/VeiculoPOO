e2bebfcc343609b3fe68a56b4e4f40a0f27095a5
SauloFerrazTC <saulopn92@gmail.com>
2023-08-21 23:22:31 -0300

    .

---
+++ codigo/src/codigo/App.java
@@ -0,0 +1,22 @@
+package codigo;
+
+public class App {
+	public static void main(String[] args) {
+		Scanner sr = new Scanner(System.in);
+		int resposta;
+		do {
+		System.out.println("Escolha criar: Carro [1] Moto [2], para parar [3]");
+		resposta = sr.nextInt();
+		
+		if(resposta==1) {
+			Carro carro = new Carro();
+			carro.ligar();
+		}
+		
+		if(resposta==2) {
+			Moto moto = new Moto();
+			moto.ligar();
+		}
+		
+		}while(resposta!=3);
+}
